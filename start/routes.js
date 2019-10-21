c61589611a09fddd02fdcf93ee734203ffb821a2
Erisvan Junior <erisvan.junior.a@gmail.com>
2019-10-21 00:19:49 -0300

    feat: add the Tweet CRUD
    Now adding the Tweet CRUD, creating new migrations, controllers, models.
    Also creating relations between models.
    CRUD with auth.

--- start/routes.js
+++ start/routes.js
@@ -19,4 +19,9 @@ const Route = use('Route')
 Route.post('/register', 'AuthController.register')
 Route.post('/authenticate', 'AuthController.authenticate')
 
-Route.get('/app', 'AppController.index').middleware(['auth'])
+Route.group(() => {
+  Route.get('/app', 'AppController.index')
+  Route.resource('/tweets', 'TweetController')
+    .apiOnly()
+    .except('update')
+}).middleware(['auth'])
